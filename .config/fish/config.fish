c20f99d69efa7366ed36f807fe33c02879e447a2
Rayan Alabbad <rayan8abbad@gmail.com>
2024-12-27 15:16:41 +0300

    Update multiple configuration files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,18 @@
 #!/usr/bin/env fish
 
-# For devices without Homebrew this will cause an error
-eval "$(/opt/homebrew/bin/brew shellenv)" 
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    # You need to install Starship first or this will cause an error
-    starship init fish | source 
+    starship init fish | source
 end
 
+source "$HOME/.cargo/env.fish"
 fish_add_path "~/.cargo/bin"
 set -x TERM xterm-256color
 ulimit -n 32768
 
 set -x LDFLAGS "-L/opt/homebrew/opt/tcl-tk/lib"
 set -x CPPFLAGS "-I/opt/homebrew/opt/tcl-tk/include"
+
+fish_add_path /Users/rayanalabbad/.spicetify
