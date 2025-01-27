c721b757734acf43512e83ce642723b232decde4
Rayan Alabbad <rayan8abbad@gmail.com>
2025-01-27 19:55:34 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,13 @@
 #!/usr/bin/env fish
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+end
+
+function figloldate
+    clear
+    date | figlet -c | lolcat -as 100
 end
 
 source "$HOME/.cargo/env.fish"
@@ -12,9 +15,4 @@ fish_add_path "~/.cargo/bin"
 set -x TERM xterm-256color
 ulimit -n 32768
 
-set -x LDFLAGS "-L/opt/homebrew/opt/tcl-tk/lib"
-set -x CPPFLAGS "-I/opt/homebrew/opt/tcl-tk/include"
-
-fish_add_path /Users/rayanalabbad/.spicetify
 fish_add_path ~/.nimble/bin
-fish_add_path /opt/homebrew/opt/llvm/bin
