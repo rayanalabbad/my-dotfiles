96a576280ef12e85bb9a9c31e7357cebc1c3ec18
Rayan Alabbad <rayan@rayanabbad.com>
2025-06-14 18:50:36 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,8 @@ if status is-interactive
     starship init fish | source
 end
 
-#source "$HOME/.cargo/env.fish"
-#fish_add_path "~/.cargo/bin"
-set -x TERM xterm-256color
 ulimit -n 32768
 
+#source "$HOME/.cargo/env.fish"
+#fish_add_path "~/.cargo/bin"
 #fish_add_path ~/.nimble/bin
