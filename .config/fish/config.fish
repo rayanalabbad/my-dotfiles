3fb4f7a5d58ceeb96a955135933cd34bf42e592d
Rayan Alabbad <rayan@rayanabbad.com>
2025-06-15 12:07:16 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,3 @@
-#!/usr/bin/env fish
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
-end
-
 #source "$HOME/.cargo/env.fish"
 #fish_add_path "~/.cargo/bin"
 #fish_add_path ~/.nimble/bin
