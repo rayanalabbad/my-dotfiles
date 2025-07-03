fbd784dd2b4fcb0af4bb8bee454eaeb91aeccea9
Rayan Alabbad <rayan@rayanabbad.com>
2025-07-03 19:38:17 +0300

    Update flake.nix

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -2,20 +2,34 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    nixpkgs = {
+    	url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    zen-browser = {
+    	url = "github:0xc000022070/zen-browser-flake";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons = { 
+    	url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+	inputs.nixpkgs.follows = "nixpkgs"; 
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, zen-browser, ... }: {
+  outputs = inputs @ { self, nixpkgs, home-manager, zen-browser, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+		
+	  #nixpkgs.config.allowUnfree = true;
 
           home-manager.nixosModules.home-manager
           {
@@ -23,7 +37,10 @@
             home-manager.useUserPackages = true;
             home-manager.users.rayanalabbad = import ./home.nix;
 	    home-manager.backupFileExtension = "backup";
-
+	    home-manager.extraSpecialArgs = {
+	      inherit inputs;
+	    };
+	
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
         ];
@@ -32,5 +49,6 @@
       	};
       };
     };
+    
   };
 }
