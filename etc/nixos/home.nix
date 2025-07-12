53dbb2f69356e1cd05b4736f91c51dead6577756
Rayan Alabbad <rayan@rayanabbad.com>
2025-07-12 05:45:44 +0300

    Update home.nix

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -17,11 +17,7 @@
 
 # The home.packages option allows you to install Nix packages into your
 # environment.
-        home.packages = [
-# # Adds the 'hello' command to your environment. It prints a friendly
-# # "Hello, world!" when run.
-# pkgs.hello
-
+        home.packages = with pkgs; [
 # # It is sometimes useful to fine-tune packages, for example, by applying
 # # overrides. You can do that directly here, just don't forget the
 # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -67,10 +63,22 @@
 #
 #  /etc/profiles/per-user/rayanalabbad/etc/profile.d/hm-session-vars.sh
 #
+
     home.sessionVariables = {
         EDITOR = "nvim";
         TERM = "xterm-256color";
+        MOZ_ENABLE_WAYLAND = "1";
     };
+
+    home.pointerCursor = {
+        gtk.enable = true;
+        x11.enable = true;
+        package = pkgs.apple-cursor;
+        name = "Apple Cursor";
+        size = 24;
+    };
+
+
 
     programs.fish = {
         enable = true;
@@ -87,7 +95,7 @@
     programs.ghostty = {
         enable = true;
         settings = {
-            font-family = "Hack Nerd Font Mono";
+            font-family = "Hack Nerd Font Mono Regular";
             font-size = "12";
             theme = "tokyonight";
 # window-decoration = false;
@@ -396,11 +404,193 @@
                     "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
                     "browser.newtabpage.activity-stream.feeds.topsites" = false;
                     "browser.newtabpage.activity-stream.enabled" = false;
-		    "browser.uidirection" = "ltr";
-		    "intl.uidirection.locale" = "ltr";
-		    "intl.uidirection.en" = "ltr";
+                    "browser.uidirection" = "ltr";
+                    "intl.uidirection.locale" = "ltr";
+                    "intl.uidirection.en" = "ltr";
                 };
             };
+        };
+    };
+
+    wayland.windowManager.hyprland = {
+        enable = true;
+        settings = {
+            input = {
+                touchpad = {
+                    natural_scroll = "true";
+                };
+            };
+            general = {
+                gaps_in = 3;
+                gaps_out = 5;
+                border_size = 1;
+                resize_on_border = "true";
+                "col.active_border" = "rgb(555555)";
+                "col.inactive_border" = "rgb(111111)";
+                allow_tearing = "false";
+                layout = "dwindle";
+            };
+            decoration = {
+                rounding = 10;
+                rounding_power = 2;
+                active_opacity = 1.0;
+                inactive_opacity = 1.0;
+                shadow = {
+                    enabled = "true";
+                    range = 4;
+                    render_power = 3;
+                    color = "rgba(1a1a1aee)";
+                };
+                blur = {
+                    enabled = "true";
+                    size = 3;
+                    passes = 1;
+                    vibrancy = 0.1696;
+                };
+            };
+            dwindle = {
+                pseudotile = "true"; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+                preserve_split = "true"; # You probably want this
+            };
+            master = {
+                new_status = "master";
+            };
+            input = {
+                kb_layout = "us,de,ara";
+#kb_variant =
+#kb_model =
+#kb_options =
+#kb_rules =
+                follow_mouse = 1;
+                sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+            };
+            gestures = {
+                workspace_swipe = "false";
+            };
+            animations = {
+                enabled = "true";
+
+                bezier = [
+                    "easeOutQuint,0.23,1,0.32,1"
+                    "easeInOutCubic,0.65,0.05,0.36,1"
+                    "linear,0,0,1,1"
+                    "almostLinear,0.5,0.5,0.75,1.0"
+                    "quick,0.15,0,0.1,1"
+                ];
+
+                animation = [
+                    "global, 1, 10, default"
+                    "border, 1, 5.39, easeOutQuint"
+                    "windows, 1, 4.79, easeOutQuint"
+                    "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+                    "windowsOut, 1, 1.49, linear, popin 87%"
+                    "fadeIn, 1, 1.73, almostLinear"
+                    "fadeOut, 1, 1.46, almostLinear"
+                    "fade, 1, 3.03, quick"
+                    "layers, 1, 3.81, easeOutQuint"
+                    "layersIn, 1, 4, easeOutQuint, fade"
+                    "layersOut, 1, 1.5, linear, fade"
+                    "fadeLayersIn, 1, 1.79, almostLinear"
+                    "fadeLayersOut, 1, 1.39, almostLinear"
+                    "workspaces, 1, 1.94, almostLinear, fade"
+                    "workspacesIn, 1, 1.21, almostLinear, fade"
+                    "workspacesOut, 1, 1.94, almostLinear, fade"
+                ];
+            };
+            "$mainMod" = "SUPER"; 
+            "$terminal" = "ghostty";
+            "$fileManager" = "dolphin";
+            "$menu" = "rofi -show drun";
+            env = [
+                "XCURSOR_THEME,macOS"
+                "XCURSOR_SIZE,24"
+                "HYPRCURSOR_SIZE,24"
+            ];
+            bind = [
+                "$mainMod, return, exec, $terminal"
+                "$mainMod, Q, killactive,"
+                "$mainMod, M, exit,"
+                "$mainMod, E, exec, $fileManager"
+                "$mainMod, V, togglefloating,"
+                "$mainMod, space, exec, $menu"
+                "$mainMod, P, pseudo," # dwindle
+                "$mainMod, J, togglesplit," # dwindle
+                "$mainMod, left, movefocus, l"
+                "$mainMod, right, movefocus, r"
+                "$mainMod, up, movefocus, u"
+                "$mainMod, down, movefocus, d"
+                "$mainMod, 1, workspace, 1"
+                "$mainMod, 2, workspace, 2"
+                "$mainMod, 3, workspace, 3"
+                "$mainMod, 4, workspace, 4"
+                "$mainMod, 5, workspace, 5"
+                "$mainMod, 6, workspace, 6"
+                "$mainMod, 7, workspace, 7"
+                "$mainMod, 8, workspace, 8"
+                "$mainMod, 9, workspace, 9"
+                "$mainMod, 0, workspace, 10"
+                "$mainMod SHIFT, 1, movetoworkspace, 1"
+                "$mainMod SHIFT, 2, movetoworkspace, 2"
+                "$mainMod SHIFT, 3, movetoworkspace, 3"
+                "$mainMod SHIFT, 4, movetoworkspace, 4"
+                "$mainMod SHIFT, 5, movetoworkspace, 5"
+                "$mainMod SHIFT, 6, movetoworkspace, 6"
+                "$mainMod SHIFT, 7, movetoworkspace, 7"
+                "$mainMod SHIFT, 8, movetoworkspace, 8"
+                "$mainMod SHIFT, 9, movetoworkspace, 9"
+                "$mainMod SHIFT, 0, movetoworkspace, 10"
+                "$mainMod, S, togglespecialworkspace, magic"
+                "$mainMod SHIFT, S, movetoworkspace, special:magic"
+                "$mainMod, mouse_down, workspace, e+1"
+                "$mainMod, mouse_up, workspace, e-1"
+            ];
+            bindm = [ 
+                "$mainMod, mouse:272, movewindow"
+                "$mainMod, mouse:273, resizewindow"
+            ];
+            /*workspace = [
+                "w[tv1], gapsout:0, gapsin:0"
+                "f[1], gapsout:0, gapsin:0"
+            ];
+            windowrule = [
+                "bordersize 0, floating:0, onworkspace:w[tv1]"
+                "rounding 0, floating:0, onworkspace:w[tv1]"
+                "bordersize 0, floating:0, onworkspace:f[1]"
+                "rounding 0, floating:0, onworkspace:f[1]"
+                "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+            ];*/
+            exec-once = [
+                "hyprpaper"
+                "hyprpanel"
+            ];
+            monitor = [
+                ",preferred,auto,auto"
+            ];
+        };
+    };
+
+    services.hyprpaper = {
+        enable = true;
+        settings = {
+            preload = [ "/home/rayanalabbad/Pictures/wallpaper.png" ];
+            wallpaper = [ ",/home/rayanalabbad/Pictures/wallpaper.png" ];
+            splash = false;
+        };
+    };
+
+    gtk = {
+        enable = true;
+        cursorTheme = {
+            name = "Apple Cursor";
+            package = pkgs.apple-cursor;
+        };
+        gtk3.extraConfig = {
+            "gtk-cursor-theme-name" = "Apple Cursor";
+        };
+        gtk4.extraConfig = {
+            Settings = ''
+                gtk-cursor-theme-name=Apple Cursor
+                '';
         };
     };
 
@@ -414,7 +604,7 @@
             clock-show-seconds = true;
             show-weekdate = true;
             icon-theme = "Papirus";
-            cursor-theme = "macOS";
+            #cursor-theme = "macOS";
 #gtk-theme = "Tokyonight-Dark";
         };
         "org/gnome/desktop/background" = {
